e72a011b7cbcbd57d88bcd79a77329fd7a3c8f44
masa-7373 <106637570+masa-7373@users.noreply.github.com>
2023-06-05 17:54:49 +0900

    commit

---
+++ first_player.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+s = []
+a = []
+for i in range(n):
+    s, a = map(str, int, input().split())
+
+
+print(s)
+print(a)
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main(){
-    int n[8];
-    int n[0] = 1;
-    int n[2] = 3;
-    int n[3] = 5;
-    int n[4] = 4;
-    int n[5] = 3;
-    int n[6] = 64;
-    int n[7] = 8;
-    int n[8] = 1;
-    for (int i = 0; i < 9; i++){
-        cout << n << endl;
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (A == 5 && B == 5 && C == 7){
+        cout << "YES" << endl;
+    }
+    else if (A == 7 && B == 5 && C == 5){
+        cout << "YES" << endl;
+    }
+    else if (A == 5 && B == 7 && C == 5){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
     }
 }
